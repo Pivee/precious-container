65cb007d6b5a32538faed48bc722ab69a6c4a513
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-05-03 03:02:24 +0530

    Create NestJS Application for Users Service

---
+++ users/src/main.ts
@@ -0,0 +1,11 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001, () => {
+    Logger.log('Users Service is running on port 3001', 'NestApplication');
+  });
+}
+bootstrap();
